940113571d879ee6a61e01b868bab1c1bb80273f
Huy <ho@cfmmedia.de>
2020-08-02 14:55:15 +0200

    added home route

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import asyncio
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 import controller
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template('home.html')
 
 
 @app.route('/brightness', methods=['GET', 'POST'])
