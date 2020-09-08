77b8e6179057aa9f008837d8caeaa3ccac36a099
Huy <huy@hhoang.dev>
2020-09-08 22:55:16 +0200

    fix templates folder not being in src

--- run.py
+++ run.py
@@ -1,15 +1,1 @@
-# import asyncio
-# from src.controller.brightness import change_brightness
-# from src.controller.temperature import change_temperature
-# from src.controller import bulb
 from src import app
-
-
-# asyncio.run(bulb.update())
-
-# b = 50 if bulb.brightness == 100 else 100
-# c = 0 if bulb.color_temp == 6500 else 100
-
-# change_brightness(b, 1)
-# change_temperature(c, 1)
-
