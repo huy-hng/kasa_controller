94a59c259824ac1f726b6d272edf5cec46345adf
Huy <huy@hhoang.dev>
2020-09-30 17:53:59 +0200

    moved SINGLE_CHANGE_DUR to vl + added colortemp class

--- src/controller/state.py
+++ src/controller/state.py
@@ -3,9 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Brightness:
-	_actual: int = 0
-	_perceived: int = 0
-
+	_actual = 0
+	_perceived = 0
 
 	@property
 	def actual(self):
@@ -19,31 +18,47 @@ class Brightness:
 	def actual(self, val):
 		self.check_valid_range(val)
 		self._actual = val
-		self._perceived = self.actual2perceived_brightness(val)
+		self._perceived = round(math.sqrt(val*100))
 
 	@perceived.setter
 	def perceived(self, val):
 		self.check_valid_range(val)
 		self._perceived = val
-		self._actual = self.perceived2actual_brightness(val)
+		self._actual = round((val ** 2) / 100) 
 
 	def check_valid_range(val):
 		if val < 0 or val > 100:
 			raise Exception('Brightness has to be between 0 and 100.')
 
-	@staticmethod
-	def perceived2actual_brightness(perceived) -> int:
-		return round((perceived ** 2) / 100) 
 
-	@staticmethod
-	def actual2perceived_brightness(actual_brightness) -> int:
-		return round(math.sqrt(actual_brightness*100))
+@dataclass
+class ColorTemperature:
+	_percent = 0
+	_kelvin = 2700
 
+	@property
+	def percent(self):
+		return self._percent
+
+	@property
+	def kelvin(self):
+		return self._kelvin
+
+	@percent.setter
+	def percent(self, val):
+		self._percent = val
+		self._kelvin = 38 * val + 2700
+
+	@kelvin.setter
+	def kelvin(self, val):
+		self._percent = (val - 2700) / 38
+		self._kelvin = val
 
 @dataclass
 class VLamp:
+	SINGLE_CHANGE_DUR = 0.12
 	brightness = Brightness()
-	color_temp = 2700
+	color_temp = ColorTemperature()
 	# on = True
 	
 	running_bright = False
