08043ce4905cafca09ac29f10f7b5fcf32392505
Huy <huy@hhoang.dev>
2020-10-02 23:47:43 +0200

    created brightness class

--- src/controller/brightness.py
+++ src/controller/brightness.py
@@ -1,78 +1,114 @@
 import math
+import time
 import asyncio
+import typing
+from dataclasses import dataclass
 
-from src.controller import bulb, helpers, vl
+from src.controller import helpers, SINGLE_CHANGE_DUR
 from src.logger import log
 
-@helpers.runner
-async def change_brightness(target_value: int, duration: int, start_value: int=None):
-	vl.running_bright = True
-	await bulb.turn_on()
-	await bulb.update()
 
-	log.warning('')
-	log.info(f'{start_value=} curr_value={bulb.brightness} {target_value=} {duration=}')
+@dataclass
+class Brightness:
+	_actual = 0
+	_perceived = 0
+	set_brightness: typing.Callable
 
-	if duration==0:
-		# change immediately
-		await set_brightness(target_value)
-		vl.running_bright = False
-		return
-	elif start_value is not None:
-		# use bulb.set_brightness bc set_brightness has a turn off feature
-		vl.brightness.perceived = start_value
-		await bulb.set_brightness(vl.brightness.actual)
+	running = False
+	should_stop = False
+	
+
+	@property
+	def actual(self):
+		return self._actual
+
+	@property
+	def perceived(self):
+		return self._perceived
+
+	@actual.setter
+	def actual(self, val):
+		val = self.check_valid_range(val)
+		log.debug(f'{val=}')
+
+		self._actual = val
+		self._perceived = round(math.sqrt(val*100))
+		self.set_brightness()
+
+	@perceived.setter
+	def perceived(self, val):
+		val = self.check_valid_range(val)
+		log.debug(f'{val=}')
+
+		self._perceived = val
+		self._actual = round((val ** 2) / 100) 
+		self.set_brightness()
+
+
+	@staticmethod
+	def check_valid_range(val):
+		if val < 0:
+			val = 0
+		elif val > 100:
+			val = 100
+		return val
+			
+
+	
+	# @helpers.thread
+	def change_brightness(self, target_value: int, duration: int, start_value: int=None):
+		log.debug(f'changing brightness to {target_value}, with duration of {duration}')
+		self.running = True
+
+		if duration==0:
+			# change immediately
+			self.perceived = target_value
+			self.running = False
+			return
+		elif start_value is not None:
+			self.perceived = start_value
+			
+		self.transition_bright(target_value, duration)
+		self.running = False
+
+
+	def transition_bright(self, target_value: int, duration: int):
+		log.debug(f'transitioning')
+
+		diff, cond = helpers.get_diff(self.perceived, target_value)
+		if diff == 0: return # return when theres no change to make
+
+		amount_of_steps, step_size = self.get_steps(duration, diff)
+		single_sleep_dur = helpers.calc_sleep_dur(duration, amount_of_steps)
+
+		log.info(f'{self.perceived=} {target_value=} {amount_of_steps=} {single_sleep_dur=}')
 		
-	await transition_bright(target_value, duration)
-	vl.running_bright = False
+		# transition
+		while cond(self.perceived):
+			self.perceived += step_size
+
+			if not cond(self.perceived):
+				# check that self.perceived doesnt overshoot target_value
+				self.perceived = target_value
+
+			if self.should_stop:
+				self.should_stop = False
+				break
+			
+			time.sleep(single_sleep_dur)
 
 
-async def set_brightness(value):
-	turn_off = False
-	if value == 0:
-		value = 1
-		turn_off = True
-
-	vl.brightness.perceived = value
-	log.debug(f'brightness={vl.brightness}')
-	await bulb.set_brightness(vl.brightness.actual)
-
-	if turn_off:
-		log.info(f'Brightness was set to 0, turning lamp off.')
-		await asyncio.sleep(0.2)
-		await bulb.turn_off()
-
-
-async def transition_bright(target_value: int, duration: int):
-	curr_value = vl.brightness.perceived # TODO: check if this line introduces bugs
-
-	diff, cond = helpers.get_diff(curr_value, target_value)
-	if diff == 0:
-		if curr_value == 0:
-			await bulb.turn_off()
-			log.info(f'turning off')
+	@staticmethod
+	def get_steps(duration, diff):
+		step_size = (diff * SINGLE_CHANGE_DUR) / (duration)
+		
+		if abs(step_size) < 2:
+			step_size = 2 if step_size > 0 else -2
+		elif step_size < 0:
+			step_size = math.floor(step_size)
 		else:
-			log.info(f'exiting, no difference')
-		return # return when theres no change to make
+			step_size = math.ceil(step_size)
 
-	amount_of_steps, step_size = get_steps(duration, diff)
-	single_sleep_dur = helpers.calc_sleep_dur(duration, amount_of_steps)
-
-	log.info(f'{curr_value=} {target_value=} {amount_of_steps=} {single_sleep_dur=}')
-	
-	await helpers.transition(curr_value, target_value, cond, set_brightness, step_size, single_sleep_dur)
-
-
-def get_steps(duration, diff):
-	step_size = (diff * vl.SINGLE_CHANGE_DUR) / (duration)
-	
-	if abs(step_size) < 2:
-		step_size = 2 if step_size > 0 else -2
-	elif step_size < 0:
-		step_size = math.floor(step_size)
-	else:
-		step_size = math.ceil(step_size)
-
-	# log.debug(f'{step_size=}')
-	amount_of_steps = math.ceil(diff / step_size)
-	return amount_of_steps, step_size
+		# log.debug(f'{step_size=}')
+		amount_of_steps = math.ceil(diff / step_size)
+		return amount_of_steps, step_size
