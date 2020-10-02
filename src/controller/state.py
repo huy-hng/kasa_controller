46f6b0f312c35e36669eb5e71f722f6d18761b55
Huy <huy@hhoang.dev>
2020-10-02 23:48:16 +0200

    vlamp with its own brightness and temp values

--- src/controller/state.py
+++ src/controller/state.py
@@ -1,68 +1,57 @@
 import math
+import asyncio
 from dataclasses import dataclass
 
-@dataclass
-class Brightness:
-	_actual = 0
-	_perceived = 0
+from src import controller
+from src.controller import helpers
+from src.controller.brightness import Brightness
+from src.controller.temperature import ColorTemperature
+from src.logger import log
 
-	@property
-	def actual(self):
-		return self._actual
-
-	@property
-	def perceived(self):
-		return self._perceived
-
-	@actual.setter
-	def actual(self, val):
-		self.check_valid_range(val)
-		self._actual = val
-		self._perceived = round(math.sqrt(val*100))
-
-	@perceived.setter
-	def perceived(self, val):
-		self.check_valid_range(val)
-		self._perceived = val
-		self._actual = round((val ** 2) / 100) 
-
-	def check_valid_range(val):
-		if val < 0 or val > 100:
-			raise Exception('Brightness has to be between 0 and 100.')
-
-
-@dataclass
-class ColorTemperature:
-	_percent = 0
-	_kelvin = 2700
-
-	@property
-	def percent(self):
-		return self._percent
-
-	@property
-	def kelvin(self):
-		return self._kelvin
-
-	@percent.setter
-	def percent(self, val):
-		self._percent = val
-		self._kelvin = 38 * val + 2700
-
-	@kelvin.setter
-	def kelvin(self, val):
-		self._percent = (val - 2700) / 38
-		self._kelvin = val
 
 @dataclass
 class VLamp:
 	SINGLE_CHANGE_DUR = 0.12
-	brightness = Brightness()
-	color_temp = ColorTemperature()
-	# on = True
-	
-	running_bright = False
-	running_temp = False
 
-	stop_bright = False
-	stop_temp = False
+	lamp_access = False
+	
+	def __init__(self):
+		self.brightness = Brightness(set_brightness=self.set_brightness)
+		self.color_temp = ColorTemperature(set_color_temp=self.set_color_temp)
+
+
+	def callback(self, location):
+		# TODO: remove, since deprecated
+		log.debug(f'callback called from {location}')
+		if self.lamp_access:
+			if location == 'brightness':
+				asyncio.run(self.set_brightness())	
+			if location == 'temperature':
+				pass
+
+	@helpers.run
+	async def set_brightness(self):
+		if not controller.bulb.is_on:
+			await controller.bulb.turn_on()
+
+		turn_off = False
+		if self.brightness.perceived == 0:
+			self.brightness.perceived = 1
+			turn_off = True
+
+		await controller.bulb.set_brightness(self.brightness.actual)
+
+		if turn_off:
+			log.info(f'Brightness was set to 0, turning lamp off.')
+			await asyncio.sleep(0.2)
+			await controller.bulb.turn_off()
+
+
+	@helpers.run
+	async def set_color_temp(value):
+		if not controller.bulb.is_on:
+			await controller.bulb.turn_on()
+
+		log.debug(f'change temp to {value}')
+		await controller.bulb.set_color_temp(self.color_temp.kelvin)
+	
