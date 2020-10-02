214aebe56ed6787082b7f65214d7aa4579c87e04
Huy <huy@hhoang.dev>
2020-10-02 23:47:53 +0200

    temperature class

--- src/controller/temperature.py
+++ src/controller/temperature.py
@@ -1,57 +1,111 @@
 import math
-import asyncio
+import typing
+import time
+from dataclasses import dataclass
 
-from src.controller import bulb, helpers, vl
-from src.controller.state import ColorTemperature
+from src.controller import helpers, SINGLE_CHANGE_DUR
 from src.logger import log
 
-@helpers.runner
-async def change_temperature(target_value: int, duration: int, start_value: int=None):
-	vl.running_temp = True
-	await bulb.turn_on()
-	await bulb.update()
 
-	log.warning('')
-
-	if duration==0:
-		await set_color_temp(target_value)
-		vl.running_temp = False
-		return
-	elif start_value is not None:
-		await set_color_temp(start_value)
-		
-	await transition_color_temp(target_value, duration)
-	vl.running_temp = False
+@dataclass
+class ColorTemperature:
+	_percent = 0
+	_kelvin = 2700
+	set_color_temp: typing.Callable
+	
+	running = False
+	should_stop = False
 
 
-async def set_color_temp(value):
-	log.debug(f'change temp to {value}')
-	vl.color_temp = value
-	await bulb.set_color_temp(vl.color_temp)
+	@property
+	def percent(self):
+		return self._percent
+
+	@property
+	def kelvin(self):
+		return self._kelvin
+
+	@percent.setter
+	def percent(self, val):
+		if val < 0: val = 0
+		elif val > 100: val = 100
+		log.debug(f'{val=}')
+
+		self._percent = val
+		self._kelvin = self.convert_to_kelvin(val)
+
+	@kelvin.setter
+	def kelvin(self, val):
+		if val < 2700: val = 2700
+		elif val > 6500: val = 6500
+		log.debug(f'{val=}')
+
+		self._percent = self.convert_to_percent(val)
+		self._kelvin = val
+
+	@staticmethod
+	def convert_to_percent(kelvin):
+		return (kelvin - 2700) / 38
+
+	@staticmethod
+	def convert_to_kelvin(percent):
+		return 38 * percent + 2700
 
 
-async def transition_color_temp(target_t: int, duration:int):
-	temp = ColorTemperature()
-	temp.percent = target_t
-	curr_value = bulb.color_temp
+	# @helpers.thread
+	def change_temperature(self, target_value: int, duration: int, start_value: int=None):
+		self.running = True
 
-	diff, cond = helpers.get_diff(curr_value, temp.kelvin)
-	if diff == 0:
-		return # return when theres no change to make
+		log.warning('')
 
-	#region calc step_size
-	step_size = (diff * vl.SINGLE_CHANGE_DUR) / duration
+		if duration==0:
+			self.percent = target_value
+			self.running = False
+			return
+		elif start_value is not None:
+			self.percent = target_value
 
-	if abs(step_size) < 100:
-		step_size = 100 if step_size > 0 else -100
+		self.transition_color_temp(target_value, duration)
+		self.running = False
 
-	step_size = helpers.round_to_nearest_100(step_size)
-	log.info(f'{step_size=}')
-	#endregion
 
-	amount_of_steps = math.ceil(diff / step_size)
-	single_sleep_dur = helpers.calc_sleep_dur(duration, amount_of_steps)
+	def transition_color_temp(self, target_percent: int, duration:int):
+		target_kelvin = self.convert_to_kelvin(target_percent)
+		diff, cond = helpers.get_diff(self.kelvin, target_kelvin)
 
-	log.info(f'{curr_value=} {temp.kelvin=} {amount_of_steps=} {single_sleep_dur=}')
+		if diff == 0: return # return when theres no change to make
 
-	await helpers.transition(curr_value, temp.kelvin, cond, set_color_temp, step_size, single_sleep_dur)
+		#region calc step_size
+		step_size = (diff * SINGLE_CHANGE_DUR) / duration
+
+		if abs(step_size) < 100:
+			step_size = 100 if step_size > 0 else -100
+
+		step_size = self.round_to_nearest_100(step_size)
+		log.info(f'{step_size=}')
+		#endregion
+
+		amount_of_steps = math.ceil(diff / step_size)
+		single_sleep_dur = helpers.calc_sleep_dur(duration, amount_of_steps)
+
+		log.debug(f'{step_size=}')
+		log.info(f'{self.kelvin=} {target_kelvin=} {duration=} {amount_of_steps=} {single_sleep_dur=}')
+
+		# transition
+		while cond(self.kelvin):
+			self.kelvin += step_size
+
+			if not cond(self.kelvin):
+				# check that self.kelvin doesnt overshoot target_kelvin
+				self.kelvin = target_kelvin
+
+			if self.should_stop:
+				self.should_stop = False
+				break
+
+			time.sleep(single_sleep_dur)
+
+
+	@staticmethod
+	def round_to_nearest_100(x, base=100):
+		return int(base * round(float(x)/base))
