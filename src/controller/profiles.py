e0ecf407098354b0493847300c583c1d1126633a
Huy <huy@hhoang.dev>
2020-10-05 00:21:03 +0200

    profiles

--- src/controller/profiles.py
+++ src/controller/profiles.py
@@ -1,13 +1,14 @@
 import datetime
+from dataclasses import dataclass
 
 import astral
-from astral.sun import sun
+from astral import sun
 
 location = astral.LocationInfo(timezone='Europe/Berlin', latitude=49.878708, longitude=8.646927)
 
 print(location.timezone)
 
-s = sun(location.observer,
+s = sun.sun(location.observer,
         date=datetime.datetime.now(),
         tzinfo=location.timezone)
 
@@ -15,3 +16,17 @@ s = sun(location.observer,
 for k, v in s.items():
   print(k, v)
 # def sunset():
+
+@dataclass
+class Sun:
+  observer = location.observer
+  date = lambda x: datetime.datetime.now()
+  tzinfo = location.timezone
+
+  def golden_hour(self):
+    return sun.twilight(self.observer, date=self.date(), direction='SETTING', tzinfo=self.tzinfo)
+
+
+s = Sun()
+for d in s.golden_hour():
+  print(d)
