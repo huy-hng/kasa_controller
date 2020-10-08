ad9299546e02c258fce1b324867ca3cf61e6becd
Huy <huy@hhoang.dev>
2020-10-08 14:54:37 +0200

    misc

--- src/test_routes.py
+++ src/test_routes.py
@@ -3,12 +3,15 @@ import requests
 
 endpoint = 'http://0.0.0.0:5000'
 
-# requests.get(endpoint+'/brightness/30')
-requests.get(endpoint+'/nvl/brightness/100')
-# time.sleep(2)
-# requests.get(endpoint+'/brightness/1/3')
-# time.sleep(2)
-# requests.get(endpoint+'/lamp')
-# time.sleep(0.5)
-requests.delete(endpoint+'/lamp')
+requests.get(endpoint+'/nvl/temp/100')
+
+
+
+# requests.get(endpoint+'/temp/0')
+requests.get(endpoint+'/brightness/1/5')
+time.sleep(1)
+requests.get(endpoint+'/brightness/100/5')
+# requests.delete(endpoint+'/brightness')
+# requests.get(endpoint+'/lamp/0')
+# requests.delete(endpoint+'/lamp')
 # requests.get(endpoint+'/brightness/100')
--- src/testing.py
+++ src/testing.py
@@ -1,22 +1,2 @@
-import time
-
-now = time.localtime()
-# print(now)
-now = {
-	'hour': now.tm_hour,
-	'min': now.tm_min
-}
-
-
-import datetime
-
-def compare_time(hour, minute):
-	now = time.localtime()
-	curr = datetime.time(now.tm_hour, now.tm_min)
-	target = datetime.time(hour, minute)
-	if curr == target:
-		return True
-	return False
-
-equal = compare_time(18, 4)
-print(equal)
+name = 'My String asd'
+print(name.lower().replace(' ', '_'))
