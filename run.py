6ab87d3a91ec4727700568b8bffe2361164f65f6
Huy <huy@hhoang.dev>
2020-10-12 19:05:18 +0200

    changed timezone

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+import os
+import time
+
 from src.app import app
 from src.periodic_runner import tl
+
+os.environ['TZ'] = 'Europe/Berlin'
+time.tzset()
 
 tl.start()
 app.run(debug=True, host='0.0.0.0')
