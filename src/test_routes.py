e2336bf3f7d7eef7c1023af70302c0a60701e922
Huy <huy@hhoang.dev>
2020-11-04 17:03:46 +0100

    .

--- src/test_routes.py
+++ src/test_routes.py
@@ -6,16 +6,5 @@ endpoint = 'http://localhost:5000'
 
 
 # requests.get(endpoint+'/nvl/brightness/100')
-requests.get(endpoint+'/nvl/temp/0')
-# time.sleep(1)
-# requests.get(endpoint+'/ nvl/temp/50/30')
-
-# requests.get(endpoint+'/nvl/brightness/1')
 # requests.get(endpoint+'/nvl/temp/0')
-# requests.get(endpoint+'/brightness/30/3')
-# requests.get(endpoint+'/lamp/0')
-
-
-# time.sleep(1)
-# requests.get(endpoint+'/brightness/100/5')
-# requests.get(endpoint+'/brightness/100')
+requests.get(endpoint+'/override/2')
