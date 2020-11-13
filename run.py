1cf7eee6a01593b6afb8a2d2e8d241a3dec58e2b
Huy <huy@hhoang.dev>
2020-11-13 20:46:27 +0100

    enable check values

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from src import periodic_runner
 os.environ['TZ'] = 'Europe/Berlin'
 time.tzset()
 
-# periodic_runner.check_values()
+periodic_runner.check_values()
 periodic_runner.check_time()
 
 app.run(host='0.0.0.0')
